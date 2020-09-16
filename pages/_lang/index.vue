794ace663132ea52bf842b887813138de89f1d12
Jérémy Saint-Prix <jeremy.saintprix@gmail.com>
2020-09-16 19:06:14 +0200

    templating

--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="app">
-    <prismic-rich-text :field="title" />
     <video autoplay loop playsinline muted class="video-background">
       <source :src="video.url" type="video/mp4" />
     </video>
+    <div class="wrapper">
+      <prismic-rich-text class="title" :field="title" />
+      <prismic-embed class="showreel" :field="showreel" />
+      <prismic-rich-text class="description" :field="description" />
+    </div>
   </div>
 </template>
 
@@ -11,31 +15,37 @@
 export default {
   async asyncData({ $prismic, params, error }) {
     try {
-      let languages = $prismic.api.data.languages;
-      let lang = { lang: languages[0].id };
+      // get lang param from url or use prismic default
+      let languages = $prismic.api.data.languages
+      let lang = { lang: languages[0].id }
       if (params.lang !== undefined || null) {
-        lang = { lang: params.lang };
+        lang = { lang: params.lang }
       }
 
-      const document = await $prismic.api.getSingle("homepage", lang);
+      const document = await $prismic.api.getSingle('homepage', lang)
       return {
         ...document.data,
-      };
+      }
     } catch (e) {
-      error({ statusCode: 404, message: "Page not found" });
+      error({ statusCode: 404, message: 'Page not found' })
     }
   },
-};
+}
 </script>
 
-<style>
+<style lang="scss">
 .app {
+  display: flex;
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
+
+  &,
+  & a {
+    color: white;
+  }
 }
 .video-background {
   object-fit: cover;
@@ -43,5 +53,13 @@ export default {
   width: 100%;
   height: 100%;
   z-index: -1;
+}
+.wrapper {
+  max-width: 80%;
+  text-align: left;
+}
+.title {
+}
+.description {
 }
 </style>
